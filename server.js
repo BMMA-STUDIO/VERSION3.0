697698b9e4981c7b427dbd0bee85b0b315a2d344
Casa Grande <casa.grande.et.senza@gmail.com>
2025-10-29 11:49:02 +0300

    Errx Midware

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const users = require('./routes/users')
 const PORT = process.env.PORT || 8383
 const path = require('path')
 
+//View Engine Setup
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'));
+
 
 //Body Parser
 app.use(express.json())
@@ -13,7 +17,18 @@ app.use(express.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
-app.use('/api/users', users)
+app.use('/users', users)
+
+//404 Rye Catcher
+app.use((req, res, next) => {
+    // Set status code 404
+    res.status(404);
+    // Render the custom 404 view
+    res.render('404', { 
+        path: req.originalUrl,
+        title: 'Error 404'
+    });
+});
 
 //RUNNING
 app.listen(PORT, () => console.log(`Test App Running on PORT:${PORT}`))
