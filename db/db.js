daa61bbef60ef3c37fc39d37e254ac5cb494ce80
Casa Grande <casa.grande.et.senza@gmail.com>
2025-11-01 09:43:48 +0300

    even more updates

---
+++ db/db.js
@@ -0,0 +1,7 @@
+//DB Array - Switch to Production Datastructure
+let users =[
+    {id:1, name: "User One", contact:"@userOneInstagram", type:"user", group:"Open"},
+    {id:2, name: "User Two", contact:"@userTwoTwitterX", type:"user", group:"Open"},
+    {id:3, name: "User Three", contact:"@userThreeFacebook", type:"user", group:"Open"},]
+
+module.exports = users;
